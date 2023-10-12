51d116fd06fad9dced9f30b605c1d55b2edf6db3
sundarraj <109932577+Sundar-guru@users.noreply.github.com>
2023-10-12 10:20:31 +0530

    added Numpy lib coed

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 #print function will execute
 
-print("Hello World")
+#print("Hello World")
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6],)
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
